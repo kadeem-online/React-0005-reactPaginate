df520a32feaf94650db0145a8cf4bb7155a960c0
kadeem254 <kadeemalexander254@gmail.com>
2024-06-24 12:06:21 +0300

    UPDATE: added base API route and slighlty cleaned up code.

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ import DOTENV from "dotenv";
 DOTENV.config();
 
 const server = EXPRESS();
-server.listen(process.env.PORT_NUMBER || 9090, () => {
-	console.log("Server running");
+const PORT_NUMBER = process.env.PORT_NUMBER || 9090;
+
+// start server
+server.listen(PORT_NUMBER, () => {
+	console.log(`Server running on port: ${PORT_NUMBER}`);
+});
+
+// set base API route
+server.get("/", (request, response) => {
+	response.status(200);
+	response.json(`API is active`);
 });
